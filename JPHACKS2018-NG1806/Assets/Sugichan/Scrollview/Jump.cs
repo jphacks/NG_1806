5273ce5d3335a6cc7f582c9f86dec51e0e21473a
LAPTOP-KAR0J5S8\suginomasashi <sgnmssuioaai@gmail.com>
2018-10-21 04:48:24 +0900

    メッセージビューからOtherへ

---
+++ JPHACKS2018-NG1806/Assets/Sugichan/Scrollview/Jump.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Jump : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Click()
+    {
+        SceneManager.LoadScene("Other", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Self_main", LoadSceneMode.Additive);
+        
+        var info = GetComponent<Info>();
+        Temp.look_name = info.sendname;
+        Temp.sendname = info.sendname;
+        Temp.look_obj = info.obj;
+        Temp.nowobjnum = info.objnum;
+        Temp.look_suc = info.suc;
+        Temp.look_fall = info.fall;
+        Temp.look_forfor = info.forfor;
+        SceneManager.UnloadSceneAsync("ScrollView");
+
+    }
+}
