918efd3d0cc39b98cb890af6277a1a860556ef14
kumachan0210 <kumachan0220ios@gmail.com>
2018-10-20 14:35:46 +0900

    スクリプト途中

---
+++ JPHACKS2018-NG1806/Assets/Scripts/Obj_text.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obj_text : MonoBehaviour {
+    public UnityEngine.UI.Text text;
+    public Now_obj no;
+    // Use this for initialization
+    void Update () {
+        text.text = PlayerPrefs.GetString(no.now + "obj");
+
+    }
+}
---
+++ JPHACKS2018-NG1806/Assets/Scripts/Touch_ob.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch_ob : MonoBehaviour {
+  
+    public int num;
+    public Now_obj ob;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (0 < Input.touchCount)
+        {
+            // タッチされている指の数だけ処理
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                // タッチ情報をコピー
+                UnityEngine.Touch t = Input.GetTouch(i);
+                // タッチしたときかどうか
+                if (t.phase == TouchPhase.Began)
+                {
+                    //タッチした位置からRayを飛ばす
+                    Ray ray = Camera.main.ScreenPointToRay(t.position);
+                    RaycastHit hit = new RaycastHit();
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        //Rayを飛ばしてあたったオブジェクトが自分自身だったら
+                        if (hit.collider.gameObject == this.gameObject)
+                        {
+                            ob.Change(num);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
