48efb95072b97c1fd68e820188c949c7b4df8735
kumachan0210 <kumachan0220ios@gmail.com>
2018-10-20 14:55:30 +0900

    押し出し

---
+++ JPHACKS2018-NG1806/Assets/Sugichan/DecideName.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DecideName : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SaveName()
+    {
+
+        PlayerPrefs.SetString("Name", GetComponent<InputField>().text);
+        PlayerPrefs.Save();
+        print(PlayerPrefs.GetString("Name",""));
+    }
+}
