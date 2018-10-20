e3442c7385d8e0b6f062ff868ef80361661b93ff
LAPTOP-KAR0J5S8\suginomasashi <sgnmssuioaai@gmail.com>
2018-10-20 15:55:44 +0900

    メッセージヴュー

--- JPHACKS2018-NG1806/Assets/Sugichan/MessageView/ViewMessage.cs
+++ JPHACKS2018-NG1806/Assets/Sugichan/MessageView/ViewMessage.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using NCMB;
+using UnityEngine.UI;
 
 public class ViewMessage : MonoBehaviour {
 
     NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("Message");
-    
+    string m;
+    public Text text;
 	// Use this for initialization
 	void Start () {
         query.WhereEqualTo("Byname", PlayerPrefs.GetString("Name", ""));
-       
+       // query.WhereEqualTo("Number", Now_obj.number);
+        query.FindAsync((List<NCMBObject> objectlist, NCMBException e) =>
+        {
+            foreach(NCMBObject message in objectlist)
+            {
+                m += message["Message"] + "\n";
+            }
+            text.text = m;
+        }
+        );
 	}
 	
 	// Update is called once per frame
