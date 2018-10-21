f1f90e5b2dd32f2004fce3b3956eb16cd1be1d2e
LAPTOP-KAR0J5S8\suginomasashi <sgnmssuioaai@gmail.com>
2018-10-21 14:57:35 +0900

    aa

---
+++ JPHACKS2018-NG1806/Assets/Sugichan/objinput/Backturn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Backturn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Return()
+    {
+        Temp.S_desu = true;
+        SceneManager.LoadScene("Self_main",LoadSceneMode.Additive);
+        SceneManager.LoadScene("Self",LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("Objinput");
+    }
+}
